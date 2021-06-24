b1f219ef62b582b7753a0f847fe4aa47b7beb127
Sizhao Liu <sizhaoliu@talend.com>
2021-06-24 14:08:35 +0000

    ads spend

---
+++ models/analytical/combined_ads_spend.sql
@@ -0,0 +1,26 @@
+WITH COMBINED_SPEND AS 
+(select * from  {{ ref('stg_bing_ads_spend') }}
+union all
+select * from  {{ ref('stg_google_ads_spend') }}),
+
+spend AS
+(SELECT 
+    DATE 
+    ,ACCOUNT
+    ,PLATFORM 
+    ,CAMPAIGN 
+    ,AD_GROUP 
+    ,SUM(IMPRESSIONS) IMPRESSIONS
+    ,SUM(CLICKS) CLICKS
+    ,SUM(SPEND) SPEND
+    ,0 as LEADS
+    ,0 as BECAME_MQL
+    ,0 as BECAME_SAL
+    ,0 as BECAME_OPPORTUNITY
+    ,0 as BECAME_CLOSED_WON
+    ,0 as PIPELINE_GENERATED
+    ,0 as PIPELINE_WON
+    FROM COMBINED_SPEND
+    group by 1,2,3,4,5)
+
+select * from spend
