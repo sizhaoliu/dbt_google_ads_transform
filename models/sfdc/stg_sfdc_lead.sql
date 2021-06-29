0cc7814f9f10bd7b0b8bb494d7f4913f72001f75
Sizhao Liu <sizhaoliu@talend.com>
2021-06-29 08:50:16 +0000

    test

--- models/sfdc/stg_sfdc_lead.sql
+++ models/sfdc/stg_sfdc_lead.sql
@@ -38,7 +38,7 @@ final as (
           then 1 else 0 end as became_mql
       ,case when   regexp_like(lead_status, '5a. Qualified.*','i')
           AND not regexp_like(lead_status, 'partner.*','i')
-          AND not regexp_like(lead_status, 'Media/Analyst*','i')
+          AND not regexp_like(lead_status, 'Media/Analyst.*','i')
           AND not regexp_like(lead_status, 'Professor.*','i')
           AND not regexp_like(lead_status, 'Student.*','i')
           and NOT regexp_like(country, 'india.*','i')
