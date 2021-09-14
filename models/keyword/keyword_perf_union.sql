bdc35359f62ac28f3787f6628ddaa5e92b75e327
Sizhao Liu <sizhaoliu@talend.com>
2021-09-14 14:13:25 +0000

    fix

--- models/keyword/keyword_perf_union.sql
+++ models/keyword/keyword_perf_union.sql
@@ -1,4 +1,6 @@
-
-select * from {{ ref('bing_ads_keyword_perf')}
-union all 
-select * from {{ ref('google_ads_keyword_perf')}
+with keyword_perf_union_data as (
+    select * from {{ ref('bing_ads_keyword_perf') }}
+    union all 
+    select * from {{ ref('google_ads_keyword_perf') }}
+)
+select * from keyword_perf_union_data
